ca8af13fb767861e738173395486a2c9a4690c30
mn042722-bay <m.n.0427@icloud.com>
2026-04-29 19:52:08 +0900

    first commit

---
+++ sample/sayHello.js
@@ -0,0 +1,6 @@
+"use strict";
+function sayHello(firstName) {
+    console.log('Hello' + firstName);
+}
+let firstName = 'Takuya';
+sayHello(firstName);
---
+++ sample/sayHello.ts
@@ -0,0 +1,6 @@
+function sayHello(firstName: String) {
+  console.log('Hello' + firstName)
+}
+
+let firstName: String = 'Takuya'
+sayHello(firstName)
