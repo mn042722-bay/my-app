119a1a10433ea2ab6cac2604278a76fe4751620a
mn042722-bay <m.n.0427@icloud.com>
2026-04-30 23:41:40 +0900

    4/30　最終更新

--- nextjs-gihyo-book/eslint.config.mjs
+++ nextjs-gihyo-book/eslint.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      "import/order": ["error", { alphabetize: { order: "asc" } }],
+    },
+  },
+  prettierConfig,
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
