08a4f145c87b38e2206d94f516339fa851972e6f
mn042722-bay <m.n.0427@icloud.com>
2026-05-01 08:51:06 +0900

    5/1　朝学習分

--- nextjs-gihyo-book/eslint.config.mjs
+++ nextjs-gihyo-book/eslint.config.mjs
@@ -1,23 +1,20 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettierConfig from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  {
-    rules: {
-      "import/order": ["error", { alphabetize: { order: "asc" } }],
-    },
+const eslintConfig = defineConfig([...nextVitals, ...nextTs, {
+  rules: {
+    "import/order": ["error", { alphabetize: { order: "asc" } }],
   },
-  prettierConfig,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+}, prettierConfig, globalIgnores([
+  ".next/**",
+  "out/**",
+  "build/**",
+  "next-env.d.ts",
+]), ...storybook.configs["flat/recommended"]]);
 
 export default eslintConfig;
